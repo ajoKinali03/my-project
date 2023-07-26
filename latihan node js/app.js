0e7416075cb76fcc3622538a015e790424efdcd1
ajoKinali03 <functionpublic10@gmail.com>
2023-07-26 15:15:49 +0700

    update latihan node js

---
+++ latihan node js/app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const asisst = require("./openAi-test");
+
+const port = 3000;
+const hostName = "127.0.0.1";
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, "Content-Type", "text/html");
+  asisst().then((value) => {
+    res.write(`<p>${value.data.choices[0].message.content}</p>`);
+    res.end();
+  }).catch((err) => {
+    res.end(err.response.data);
+  });
+});
+
+server.listen(port, hostName, () => {
+  console.log(`server run at http://${hostName}:${port}`);
+});
