6c613fb56e27f21db0c1da81beae327c1dc5ef5f
ajoKinali03 <functionpublic10@gmail.com>
2023-08-05 16:36:47 +0700

    add file di file = formating docx > utils > pusta pengolahan data

---
+++ formating-docx/utils/pusat_pengolah_data.js
@@ -0,0 +1,13 @@
+const manageText = (data) => {
+  console.log(JSON.parse(data));
+
+  // menjalankan cek sentance case kalimat
+
+  // menjalankan cek bahasa
+
+  // mejalankan cek tab dan jumlahnya
+
+  // melakakuan perbandngan dengan data asli untuk mendapatkan kode text
+};
+
+module.exports = {manageText};
