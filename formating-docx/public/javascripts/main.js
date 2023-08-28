bcbaaaf17c30b49ce958602e4fd68c47d57b100d
ajo kinali <105143492+ajoKinali03@users.noreply.github.com>
2023-08-28 08:18:13 +0700

    Update main.js

--- formating-docx/public/javascripts/main.js
+++ formating-docx/public/javascripts/main.js
@@ -1,17 +1,17 @@
 const inpt = document.getElementById("form-input");
 const inputPost = document.getElementById("input-post");
 
-// fetch("http://localhost:3000/home/api").then((res) =>
-//   res
-//     .json()
-//     .then((data) => {
-//       data.forEach((e, i) => {
-//         console.log(e.teks)
-//         inpt.value += e.teks.join("");
-//       });
-//     })
-//     .catch((err) => console.error(err))
-// );
+ fetch("http://localhost:3000/home/api").then((res) =>
+   res
+     .json()
+     .then((data) => {
+       data.forEach((e, i) => {
+         console.log(e.teks)
+         inpt.value += e.teks.join("");
+       });
+     })
+     .catch((err) => console.error(err))
+ );
 
 document.addEventListener("keypress", () => {
   if (event.code == "Enter") {
