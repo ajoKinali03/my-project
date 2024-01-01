6c5804c00bfd7d5b366cf1bcb202a2bc1d1771b9
ajoKinali03 <functionpublic10@gmail.com>
2024-01-01 12:29:01 +0700

    no update

--- formating-docx/utils/data_tipe_teks.js
+++ formating-docx/utils/data_tipe_teks.js
@@ -2,7 +2,8 @@
 
 // function cek penomoran
 const cekNmr = (arrTks) => {
-  arrTks = arrTks.join(",").split(",");
+  // arrTks = arrTks.join(",").split(",");
+  // console.log(arrTks);
   const ktgrNmr = arrObjNmr();
   let result = false;
   let tipe = null;
@@ -10,24 +11,25 @@ const cekNmr = (arrTks) => {
   let count = 0;
   let arrKos = [];
   arrTks.forEach((e, i) => {
-    // e = e.join("");
+    e = e.join(",").split(",");
+    console.log(e);
     if (e != "\t" && e != "") {
       count++;
     }
     ktgrNmr.forEach((cek, idx) => {
-      console.log(arrTks[i + 1]);
-      if (cek.cekTipe(e) && arrTks[i + 1] == cek.dot && count == 1) {
+      console.log(arrTks[i][1]);
+      if (cek.cekTipe(e) && arrTks[i][1] == cek.dot && count == 1) {
         count == 0;
-        // result = true;
-        // tipe = cek.tipe(e);
-        // tingkatPenomoran = cek.tingkat;
-        console.log({ cekNmr: true, tipe: cek.tipe(e), tingkat: cek.tingkat });
-        arrKos.push({ cekNmr: true, tipe: cek.tipe(e), tingkat: cek.tingkat });
+        result = true;
+        tipe = cek.tipe(e);
+        tingkatPenomoran = cek.tingkat;
+        // console.log({ cekNmr: true, tipe: cek.tipe(e), tingkat: cek.tingkat });
+        // arrKos.push({ cekNmr: true, tipe: cek.tipe(e), tingkat: cek.tingkat });
       }
     });
   });
-  // return { cekNmr: result, tipe: tipe, tingkat: tingkatPenomoran };
-  return arrKos;
+  return { cekNmr: result, tipe: tipe, tingkat: tingkatPenomoran };
+  // return arrKos;
 };
 
 // fungsi menampung arr objek cek penomoran
